87d54c4d6eee087f6be466b9a20eb4e582181aba
Youze Tang <tangyouze@gmail.com>
2015-08-08 12:07:28 +0800

    update oss

--- tushare/stock/cons.py
+++ tushare/stock/cons.py
@@ -18,22 +18,22 @@ PAGE_NUM = [38, 60, 80, 100]
 FORMAT = lambda x: '%.2f' % x
 DOMAINS = {'sina': 'sina.com.cn', 'sinahq': 'sinajs.cn',
            'ifeng': 'ifeng.com', 'sf': 'finance.sina.com.cn',
-           'vsf': 'vip.stock.finance.sina.com.cn', 
+           'vsf': 'vip.stock.finance.sina.com.cn',
            'idx': 'www.csindex.com.cn', '163': 'money.163.com',
            'em': 'eastmoney.com', 'sseq': 'query.sse.com.cn',
            'sse': 'www.sse.com.cn', 'szse': 'www.szse.cn',
-           'oss': 'waditu.com',
+           'oss': 'tudata.oss-cn-beijing.aliyuncs.com',
            'shibor': 'www.shibor.org'}
 PAGES = {'fd': 'index.phtml', 'dl': 'downxls.php', 'jv': 'json_v2.php',
-         'cpt': 'newFLJK.php', 'ids': 'newSinaHy.php', 'lnews':'rollnews_ch_out_interface.php',
-         'ntinfo':'vCB_BulletinGather.php', 'hs300b':'000300cons.xls',
-         'hs300w':'000300closeweight.xls','sz50b':'000016cons.xls',
-         'dp':'all_fpya.php', '163dp':'fpyg.html',
-         'emxsg':'JS.aspx', '163fh':'jjcgph.php',
-         'newstock':'vRPD_NewStockIssue.php', 'zz500b':'000905cons.xls',
-         't_ticks':'vMS_tradedetail.php', 'dw': 'downLoad.html',
-         'qmd':'queryMargin.do', 'szsefc':'FrontController.szse',
-         'ssecq':'commonQuery.do'}
+         'cpt': 'newFLJK.php', 'ids': 'newSinaHy.php', 'lnews': 'rollnews_ch_out_interface.php',
+         'ntinfo': 'vCB_BulletinGather.php', 'hs300b': '000300cons.xls',
+         'hs300w': '000300closeweight.xls', 'sz50b': '000016cons.xls',
+         'dp': 'all_fpya.php', '163dp': 'fpyg.html',
+         'emxsg': 'JS.aspx', '163fh': 'jjcgph.php',
+         'newstock': 'vRPD_NewStockIssue.php', 'zz500b': '000905cons.xls',
+         't_ticks': 'vMS_tradedetail.php', 'dw': 'downLoad.html',
+         'qmd': 'queryMargin.do', 'szsefc': 'FrontController.szse',
+         'ssecq': 'commonQuery.do'}
 TICK_COLUMNS = ['time', 'price', 'change', 'volume', 'amount', 'type']
 TODAY_TICK_COLUMNS = ['time', 'price', 'pchange', 'change', 'volume', 'amount', 'type']
 DAY_TRADING_COLUMNS = ['code', 'symbol', 'name', 'changepercent',
@@ -57,9 +57,9 @@ INX_DAY_PRICE_COLUMNS = ['date', 'open', 'high', 'close', 'low', 'volume', 'pric
 LIVE_DATA_COLS = ['name', 'open', 'pre_close', 'price', 'high', 'low', 'bid', 'ask', 'volume', 'amount',
                   'b1_v', 'b1_p', 'b2_v', 'b2_p', 'b3_v', 'b3_p', 'b4_v', 'b4_p', 'b5_v', 'b5_p',
                   'a1_v', 'a1_p', 'a2_v', 'a2_p', 'a3_v', 'a3_p', 'a4_v', 'a4_p', 'a5_v', 'a5_p', 'date', 'time', 's']
-FOR_CLASSIFY_B_COLS = ['code','name']
-FOR_CLASSIFY_W_COLS = ['date','code','weight']
-THE_FIELDS = ['code','symbol','name','changepercent','trade','open','high','low','settlement','volume','turnoverratio']
+FOR_CLASSIFY_B_COLS = ['code', 'name']
+FOR_CLASSIFY_W_COLS = ['date', 'code', 'weight']
+THE_FIELDS = ['code', 'symbol', 'name', 'changepercent', 'trade', 'open', 'high', 'low', 'settlement', 'volume', 'turnoverratio']
 TICK_PRICE_URL = '%smarket.%s/%s?date=%s&symbol=%s'
 TODAY_TICKS_PAGE_URL = '%s%s/quotes_service/api/%s/CN_Transactions.getAllPageTime?date=%s&symbol=%s'
 TODAY_TICKS_URL = '%s%s/quotes_service/view/%s?symbol=%s&date=%s&page=%s'
@@ -74,10 +74,10 @@ OPERATION_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/operation/%s?s_i=&s_a=&s_c=&
 GROWTH_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/grow/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
 DEBTPAYING_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/debtpaying/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
 CASHFLOW_URL = '%s%s/q/go.php/vFinanceAnalyze/kind/cashflow/%s?s_i=&s_a=&s_c=&reportdate=%s&quarter=%s&p=%s&num=%s'
-SHIBOR_TYPE ={'Shibor': 'Shibor数据', 'Quote': '报价数据', 'Tendency': 'Shibor均值数据',
-              'LPR': 'LPR数据', 'LPR_Tendency': 'LPR均值数据'}
+SHIBOR_TYPE = {'Shibor': 'Shibor数据', 'Quote': '报价数据', 'Tendency': 'Shibor均值数据',
+               'LPR': 'LPR数据', 'LPR_Tendency': 'LPR均值数据'}
 SHIBOR_DATA_URL = '%s%s/shibor/web/html/%s?nameNew=Historical_%s_Data_%s.xls&downLoadPath=data&nameOld=%s%s.xls&shiborSrc=http://www.shibor.org/shibor/'
-ALL_STOCK_BASICS_FILE = '%s%s/all.csv'%(P_TYPE['http'], DOMAINS['oss'])
+ALL_STOCK_BASICS_FILE = '%s%s/all.csv' % (P_TYPE['http'], DOMAINS['oss'])
 SINA_CONCEPTS_INDEX_URL = '%smoney.%s/q/view/%s?param=class'
 SINA_INDUSTRY_INDEX_URL = '%s%s/q/view/%s'
 SINA_DATA_DETAIL_URL = '%s%s/quotes_service/api/%s/Market_Center.getHQNodeData?page=1&num=400&sort=symbol&asc=1&node=%s&symbol=&_s_r_a=page'
@@ -91,16 +91,16 @@ SSEQ_CQ_REF_URL = '%s%s/assortment/stock/list/name'
 ALL_STK_URL = '%s%s/all.csv'
 SHIBOR_COLS = ['date', 'ON', '1W', '2W', '1M', '3M', '6M', '9M', '1Y']
 QUOTE_COLS = ['date', 'bank', 'ON_B', 'ON_A', '1W_B', '1W_A', '2W_B', '2W_A', '1M_B', '1M_A',
-                    '3M_B', '3M_A', '6M_B', '6M_A', '9M_B', '9M_A', '1Y_B', '1Y_A']
-SHIBOR_MA_COLS = ['date', 'ON_5', 'ON_10', 'ON_20', '1W_5', '1W_10', '1W_20','2W_5', '2W_10', '2W_20',
+              '3M_B', '3M_A', '6M_B', '6M_A', '9M_B', '9M_A', '1Y_B', '1Y_A']
+SHIBOR_MA_COLS = ['date', 'ON_5', 'ON_10', 'ON_20', '1W_5', '1W_10', '1W_20', '2W_5', '2W_10', '2W_20',
                   '1M_5', '1M_10', '1M_20', '3M_5', '3M_10', '3M_20', '6M_5', '6M_10', '6M_20',
-                  '9M_5', '9M_10', '9M_20','1Y_5', '1Y_10', '1Y_20']
+                  '9M_5', '9M_10', '9M_20', '1Y_5', '1Y_10', '1Y_20']
 LPR_COLS = ['date', '1Y']
 LPR_MA_COLS = ['date', '1Y_5', '1Y_10', '1Y_20']
 INDEX_HEADER = 'code,name,open,preclose,close,high,low,0,0,volume,amount,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,d,c,3\n'
 INDEX_COLS = ['code', 'name', 'change', 'open', 'preclose', 'close', 'high', 'low', 'volume', 'amount']
 HIST_FQ_COLS = ['date', 'open', 'high', 'close', 'low', 'volume', 'amount', 'factor']
-HIST_FQ_FACTOR_COLS = ['code','value']
+HIST_FQ_FACTOR_COLS = ['code', 'value']
 DATA_GETTING_TIPS = '[Getting data:]'
 DATA_GETTING_FLAG = '#'
 DATA_ROWS_TIPS = '%s rows data found.Please wait for a moment.'
@@ -112,31 +112,39 @@ TOP_PARAS_MSG = 'top有误，请输入整数或all.'
 LHB_MSG = '周期输入有误，请输入数字5、10、30或60'
 
 import sys
+
 PY3 = (sys.version_info[0] >= 3)
+
+
 def _write_head():
     sys.stdout.write(DATA_GETTING_TIPS)
     sys.stdout.flush()
 
+
 def _write_console():
     sys.stdout.write(DATA_GETTING_FLAG)
     sys.stdout.flush()
-    
+
+
 def _write_tips(tip):
-    sys.stdout.write(DATA_ROWS_TIPS%tip)
+    sys.stdout.write(DATA_ROWS_TIPS % tip)
     sys.stdout.flush()
+
 
 def _write_msg(msg):
     sys.stdout.write(msg)
     sys.stdout.flush()
-    
+
+
 def _check_input(year, quarter):
-    if isinstance(year, str) or year < 1989 :
+    if isinstance(year, str) or year < 1989:
         raise TypeError(DATE_CHK_MSG)
     elif quarter is None or isinstance(quarter, str) or quarter not in [1, 2, 3, 4]:
         raise TypeError(DATE_CHK_Q_MSG)
     else:
         return True
-    
+
+
 def _check_lhb_input(last):
     if last not in [5, 10, 30, 60]:
         raise TypeError(LHB_MSG)
